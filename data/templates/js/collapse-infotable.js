f987cad7181fc14c471fb6a0486d8ae406cb82e7
zoroz <zarko.oroz@symphony.is>
2018-06-29 14:08:50 +0200

    Mobile rendering: Updated script to remove empty paragraphs

--- data/templates/js/collapse-infotable.js
+++ data/templates/js/collapse-infotable.js
@@ -1,4 +1,7 @@
 $(function() {
+	// Remove all empty paragraphs
+	$('.mw-parser-output p').filter(function () { return $.trim(this.innerHTML) == "" }).remove();
+
 	// Add a wrapper div around first table infobox
 	$(".infobox:first").wrap("<div class='infobox-toggle-wrapper'><div class='infobox-toggle'></div></div>");
 
